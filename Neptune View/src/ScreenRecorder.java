0a325cee0ec5192dcfe92f161fbfd35f87ffe5ca
Great Ark Studios <ericz2241@gmail.com>
2013-09-01 15:07:17 -0400

    Updates to Drawing.java and updated to java se 7

--- Neptune View/src/ScreenRecorder.java
+++ Neptune View/src/ScreenRecorder.java
@@ -17,6 +17,8 @@
  * Copyright 2009, 2010, 2011, 2012, 2013 Caprica Software Limited.
  */
 
+
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -38,91 +40,87 @@ import uk.co.caprica.vlcj.test.VlcjTest;
  */
 public class ScreenRecorder extends VlcjTest {
 
-	private static final String[] OPTIONS = { "--no-plugins-cache",
-			"--no-video-title-show", "--quiet", "--quiet-synchro", "--intf",
-			"dummy" };
+    private static final String[] OPTIONS = {
+        "--no-plugins-cache",
+        "--no-video-title-show",
+        "--quiet",
+        "--quiet-synchro",
+        "--intf",
+        "dummy"
+    };
 
-	private static final String MRL = "screen://";
-	private static final String SOUT = ":sout=#transcode{vcodec=FLV1e,vb=%d,scale=%f}:duplicate{dst=file{dst=%s}}";
-	private static final String FPS = ":screen-fps=%d";
-	private static final String CACHING = ":screen-caching=%d";
+    private static final String MRL     = "screen://";
+    private static final String SOUT    = ":sout=#transcode{vcodec=FLV1,vb=%d,scale=%f}:duplicate{dst=file{dst=%s}}";
+    private static final String FPS     = ":screen-fps=%d";
+    private static final String CACHING = ":screen-caching=%d";
 
-	private static final int fps = 50;
-	private static final int caching = 500;
-	private static final int bits = 4096;
-	private static final float scale = 0.5f;
+    private static final int    fps     = 20;
+    private static final int    caching = 500;
+    private static final int    bits    = 4096;
+    private static final float  scale   = 0.5f;
 
-	private final MediaPlayerFactory mediaPlayerFactory;
-	private final MediaPlayer mediaPlayer;
+    private final MediaPlayerFactory mediaPlayerFactory;
+    private final MediaPlayer mediaPlayer;
 
-	private final JFrame frame;
+    private final JFrame frame;
 
-	public static void main(String[] args) {
-		NativeLibrary.addSearchPath("vlc",
+    public static void main(String[] args) {
+    	NativeLibrary.addSearchPath("vlc",
 				"/Applications/VLC.app/Contents/MacOS/lib");
 		System.setProperty("jna.library.path",
 				"/Applications/VLC.app/Contents/MacOS/lib");
+        final String destination = "record2.flv";
 
-		final String destination = "record1.flv";
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ScreenRecorder(destination);
+            }
+        });
+    }
 
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new ScreenRecorder(destination);
-			}
-		});
-	}
+    public ScreenRecorder(final String destination) {
+        mediaPlayerFactory = new MediaPlayerFactory(OPTIONS);
+        mediaPlayer = mediaPlayerFactory.newHeadlessMediaPlayer();
+        JPanel cp = new JPanel();
+        JButton recordButton = new JButton("Record");
+        cp.add(recordButton);
+        frame = new JFrame("vlcj");
+        frame.setContentPane(cp);
+        frame.setLocation(10, 10);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                stop();
+            }
+        });
+        recordButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                go(destination);
+            }
+        });
+        frame.setVisible(true);
+    }
 
-	public ScreenRecorder(final String destination) {
-		mediaPlayerFactory = new MediaPlayerFactory(OPTIONS);
-		mediaPlayer = mediaPlayerFactory.newHeadlessMediaPlayer();
-		JPanel cp = new JPanel();
-		JButton recordButton = new JButton("Record");
-		JButton stopButton = new JButton("Stop");
-		cp.add(recordButton);
-		cp.add(stopButton);
-		frame = new JFrame("vlcj");
-		frame.setContentPane(cp);
-		frame.setLocation(10, 10);
-		frame.pack();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private void go(String destination) {
+        frame.setState(JFrame.ICONIFIED);
+        mediaPlayer.playMedia(MRL, getMediaOptions(destination));
+    }
 
-		frame.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				stop();
-			}
-		});
-		recordButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				go(destination);
-			}
-		});
-		frame.setVisible(true);
+    private void stop() {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayerFactory.release();
+    }
 
-		stopButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				stop();
-			}
-		});
-		
-	}
-
-	private void go(String destination) {
-		frame.setState(JFrame.NORMAL);
-		mediaPlayer.playMedia(MRL, getMediaOptions(destination));
-	}
-
-	private void stop() {
-		mediaPlayer.stop();
-		mediaPlayer.release();
-		mediaPlayerFactory.release();
-
-	}
-
-	private String[] getMediaOptions(String destination) {
-		return new String[] { String.format(SOUT, bits, scale, destination),
-				String.format(FPS, fps), String.format(CACHING, caching) };
-	}
+    private String[] getMediaOptions(String destination) {
+        return new String[] {
+            String.format(SOUT, bits, scale, destination),
+            String.format(FPS, fps),
+            String.format(CACHING, caching)
+        };
+    }
 }
